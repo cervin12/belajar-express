6ea53e7394ee0e5813431c0ae473c5171feeb4dc
Cervin <houcervin55@gmail.com>
2022-12-26 22:58:08 +0700

    express urlencoded

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const app = express();
 
 app.use(express.json());
 
+app.use(express.urlencoded({ extended:true }));
+
 app.get('/',(request, response) => {
     console.log(request.body)
     response.send(`Hello ${request.query.name}, umur aku ${request.body.umur}`);
+})
+app.post('/',(request, response) => {
+    console.log(request.body)
+    response.send(`Hello ${request.body.name}, umur aku ${request.body.umur}`);
 })
 
 app.listen(3000, ()=>{
